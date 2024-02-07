a3e5ca024ed8c8e089099d25cde41f852068d6a9
Sidd@2124 <siddhumsd@gmail.com>
2024-02-07 21:00:16 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Details from './Components/Details';
 import './App.css';
+import Context from './Components/Context/Context';
+import FormerEntry from './Components/Entries/FormerEntry';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    FirstArray: [
+      
+    ]
+  };
+
+  
+  componentDidMount() {
+    const You = JSON.parse(localStorage.getItem("Info"));
+    if (You) {
+      this.setState({ FirstArray: You });
+    }
+  }
+
+ 
+  
+
+
+  Happend = (K) => {
+    const { FirstArray } = this.state;
+    const filteredArray = FirstArray.filter((each) => each.id !== K);
+    this.setState({ FirstArray: filteredArray });
+    localStorage.setItem("Info", JSON.stringify(filteredArray));
+  };
+  
+  
+  
+
+  Update = (J) => {
+    this.setState((prevState) => {
+      const updatedArray = [...prevState.FirstArray, J];
+      localStorage.setItem("Info", JSON.stringify(updatedArray));
+      return { FirstArray: updatedArray };
+    });
+  };
+  
+  render() {
+    const { FirstArray } = this.state;
+    return (
+      <div className="App">
+        <Context.Provider value={{ FinelArray: FirstArray, NewArray: this.Update,FinelRemove:this.Happend }}>  
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/" component={Details} />
+              <Route exact path="/Entries" component={FormerEntry} />
+            </Switch>
+          </BrowserRouter>
+        </Context.Provider>
+      </div>
+    );
+  }
 }
 
 export default App;
